7465facaeaf00b7e145c93715b3bdaa49a3bb795
RohitBhojak <rohitbhojak99@gmail.com>
2024-12-07 15:32:04 +0000

    Add function to generate array and display in canvas

--- sorting/scripts/main.js
+++ sorting/scripts/main.js
@@ -4,13 +4,27 @@ let bar_height = [];
 
 let inputSize = document.querySelector("#size");
 let inputSpeed = document.querySelector("#speed");
+let generateButton = document.querySelector("#generate");
 
 inputSize.addEventListener("input", () => {
-    
+    bar = [];
+    bar_height = [];
+    generateArr(inputSize.value);
+})
+
+generateButton.addEventListener("click", () => {
+    bar = [];
+    bar_height = [];
+    generateArr(inputSize.value);
 })
 
 function generateArr(size) {
+    canvas.innerHTML = "";
     for (let i = 0; i < size; i++) {
-        div.push(Math.floor(Math.random() * 100));
+        bar_height.push(Math.floor(Math.random() * 95) + 5);
+        bar[i] = document.createElement("div");
+        bar[i].style.height = `${bar_height[i]}%`;
+        bar[i].classList.add("bar");
+        canvas.appendChild(bar[i]);
     }
 }
